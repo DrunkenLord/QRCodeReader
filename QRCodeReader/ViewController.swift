3dd08fc2c4bae6d04e4663df473b8a459bc85f91
Mehul Singhal <mehulbtp@gmail.com>
2017-09-04 22:10:10 +0530

    QR Code reader for any application

--- QRCodeReader/ViewController.swift
+++ QRCodeReader/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    @IBOutlet weak var square: UIImageView!
+    
+    var video = AVCaptureVideoPreviewLayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Creating session
+        let session = AVCaptureSession()
+        //Define Capture Session
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do
+        {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+            
+        }
+        catch
+        {
+            print("Error")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: square)
+        
+        session.startRunning()
+        
+
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects != nil && metadataObjects.count != nil
+        {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+            {
+                if object.type == AVMetadataObjectTypeQRCode
+                {
+                    let alert = UIAlertController(title: "QR Code Scanned", message: "Welcome", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Awesome", style: .default, handler: nil))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
